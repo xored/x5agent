9f5566e4441c14ebcbb27036fc8182561a4a9433
Ivan Inozemtsev <ivan.inozemtsev@xored.com>
2011-08-08 17:08:10 +0700

    Added throw declaration of IOException to X5Transport#initialize and X5Transport#send

--- core/x5agent/src/com/xored/x5agent/core/X5Transport.java
+++ core/x5agent/src/com/xored/x5agent/core/X5Transport.java
@@ -1,5 +1,6 @@
 package com.xored.x5agent.core;
 
+import java.io.IOException;
 import java.util.Map;
 
 import com.xored.x5.X5Request;
@@ -9,8 +10,11 @@ public interface X5Transport {
 	/**
 	 * Will be called after object’s construction, but before any actual usage
 	 * of the object.
+	 * 
+	 * @throws IOException when parameters are valid, but transport could not start. In this case X5 agent will try to restart transport later
+	 * IllegalArgumentException when parameters are invalid. X5 agent won't try to reinitialize this transport until parameters are changed
 	 */
-	void initialize(Map<String, String> parameters);
+	void initialize(Map<String, String> parameters) throws IOException;
 
 	/**
 	 * Must be thread-safe.
@@ -18,7 +22,7 @@ public interface X5Transport {
 	 * @param request
 	 *            Request to send to the destination.
 	 */
-	X5Response send(X5Request request);
+	X5Response send(X5Request request) throws IOException;
 
 	/**
 	 * When X5 does not need transport object any longer, it calls dispose on
